8f17a89ff2b4b63ffaee680a242b651c1f39fe8e
Lawrence Abaeo <labaeo@gmail.com>
2014-01-09 20:55:04 -0800

    Added more user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,18 @@ describe User do
   it "has a valid factory" do
     create(:user).should be_valid
   end
+  it "is invalid without an email" do 
+    build(:user, email: nil).should_not be_valid
+  end
+  it "is invalid without a password" do 
+    build(:user, password: nil).should_not be_valid
+  end
+  it "is invalid when password doesn't match password confirmation" do 
+    build(:user, password: "pooppoop").should_not be_valid
+  end
+  # NOTE: can't test password_confirmation as it's not really part of the model, but part of the controller
+  # it "is invalid without a password confirmation value" do 
+  #   build(:user, password_confirmation: nil).should_not be_valid 
+  # end
+
 end
